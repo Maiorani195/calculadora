c4ebf72dde332bbbb963c3c23c113d839d799263
Fernando <fernandomaioranicosta6@gmail.com>
2026-01-27 22:25:53 -0300

    first commit

---
+++ import meu_modulo_calculadora.py
@@ -0,0 +1,27 @@
+import meu_modulo_calculadora.py
+def main(): 
+    a = 10
+    b = 5
+    print("Soma:", meu_modulo_calculadora.somar(a, b))
+    print("Subtração:", meu_modulo_calculadora.subtrair(a, b))
+    print("Multiplicação:", meu_modulo_calculadora.multiplicar(a, b))
+    print("Divisão:", meu_modulo_calculadora.dividir(a, b))
+    #meu_modulo_calculadora.py
+# def calcular(num1, num2, operacao):
+   # match operacao:
+        #case "+":
+          #  return num1 + num2
+       # case "-":
+       #     return num1 - num2
+       # case "*":
+      #      return num1 * num2
+      #  case "/":
+       #     return num1 / num2
+
+   # case "*":
+    #  res = num1 * num2
+
+    #  case "/":
+      #res = num1 / num2
+
+   # print(f"Resultado é igual  a {res}")
